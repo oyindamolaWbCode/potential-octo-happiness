55cad6566eaddf02b172e4410a2d306808dede68
oyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-06-02 20:11:28 +0100

    [update on Js]

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-document.getElementById('count').innerText = 7
+//document.getElementById('count').innerText = 50
+
+
+
+
+let myNumber = 22
+
+console.log(myNumber)
+
+let myAge = 21
+
+let myHumanDogRatio = 21
+
+let myDogAge = myAge * myHumanDogRatio
+
+console.log(myDogAge)
+
+let theSense = 50
+
+// let theSense = theSense + 100
+
+theSense = theSense + 100
+
+console.log(theSense)
+
+// let theSense = theSense - 25
+
+theSense = theSense - 25
+
+console.log(theSense)
+
+// let theSense = theSense - 70
+
+theSense = theSense + 70
+
+console.log(theSense)
