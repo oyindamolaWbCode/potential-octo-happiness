6d0114d59cbaa5bc4ebf32b439adf99d3bbb8661
oyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-07-07 23:39:57 +0100

    NEW PARAMETERS AND ARGUEMENTS

--- index.js
+++ index.js
@@ -111,6 +111,124 @@ console.log(message)
 
  alert(myGreeting)
 
+ //Parameter and Arguement
+//parameters
+//parameters are placed in our functions t0 make it dynamic
+//parameters are the word passed immediately after the function name ()
 
- // More On Functions
+ function favGame(game){
+    console.log(`My favorite game is ${game}`)
+ }
+ favGame("Candy Crush");
+ favGame("Sonia"); 
+ favGame("Temple Run");
 
+ //Arguement
+//is the invoking of words in the parenthesis after the function is been called
+// how to insert multiple parameters separing them with commas
+
+function ninjaTitle(name, weapon){
+    console.log(`${name} uses the ${weapon}`)
+}
+ninjaTitle("Nikim", "Buzza");
+
+// For Numbers
+function remainder(num1, num2){
+    console.log(num1 % num2);
+}
+remainder(180, 70);
+
+// Example
+function favFood(response){
+    alert(`Your Fav food is ${response}`)
+}
+
+favFood()
+
+if (alert != favFood()){
+    alert("Please input your fav food...")
+}else{
+    console.log("Thank you !")
+}
+// More On Functions
+
+//  const  {sumOfNumbers}  = assignment;
+//  const maybe = sumOfNumbers === undefined ? test.skip : test;
+ 
+// let sumOfNumbers = 0;
+
+//  maybe('Challenge - 1 : my sumNumbers function can sum an array of numbers', () => {
+     
+    //  const numbers = [
+    //      68,
+    //      -68,
+    //      27,
+    //      94,
+    //      72,
+    //      -25,
+    //      -51,
+    //      32,
+    //      10,
+    //      64,
+    //      -94,
+    //      4,
+    //      34,
+    //      -86,
+    //      90,
+    //      81,
+    //      20,
+    //      -56,
+    //      -91,
+    //      -50
+    //  ]; 
+    //  //expect(sumOfNumbers(numbers)).toBe(75);
+
+    //  for(let i = 0; i < numbers.length; i++){
+    //    sumOfNumbers = sumOfNumbers + numbers[i]
+    //  console.log(sumOfNumbers);
+    //  }
+     
+//  });
+//  console.log(maybe);
+
+const assignment = require('../src/assignment.js');
+
+const { countEvenNumbers } = assignment;
+const maybe = countEvenNumbers === undefined ? test.skip : test;
+
+maybe('Challenge - 2 : my countEvenNumbers function can count even numbers in array of numbers', () => {
+
+    let countEvenNumbers = 0;
+
+    const number = [
+        17,
+        0,
+        67,
+        41,
+        49,
+        21,
+        1,
+        86,
+        76,
+        2,
+        54,
+        14,
+        93,
+        84,
+        14,
+        77,
+        35,
+        7,
+        29,
+        64
+    ]
+    const even = [];
+
+    number.forEach(numbers => {
+      if (numbers % 2 === 0) {
+        even.push(number);
+      }
+    });
+    console.log(even);
+   // expect(countEvenNumbers(numbers)).toBe(9);
+});
